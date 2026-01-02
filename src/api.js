43c2aadb1fad09985d84fd604c47a20d927952e4
Hicham <webnetmaster01@gmail.com>
2026-01-03 00:20:46 +0100

    Initial commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://archivage-doc.up.railway.app/api",
+    baseURL: "https://archivage-doc.up.railway.app/api/",
 });
 
 export default API;

b7870fee0715314a71e01a5aabe74354be38eda3
hichamDao <118081765+hichamDao@users.noreply.github.com>
2026-01-03 13:06:17 +0100

    Update API base URL for axios instance

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://archivageapp-production.up.railway.app/api/",
+    baseURL: "https://archivageapp-production.up.railway.app",
 });
 
 export default API;
